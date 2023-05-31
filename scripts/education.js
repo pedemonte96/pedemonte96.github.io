7a218c5cf2f2069f88ec81ea7a3603716bc68255
Martí <pedemonte96@gmail.com>
2023-05-30 20:24:46 -0400

    education, experience and skills now dynamically created through javascript

---
+++ scripts/education.js
@@ -0,0 +1,133 @@
+export var educationData = [
+    {
+        "degree" : "Master Thesis in Experimental Particle Physics",
+        "institution" : "Massachusetts Institute of Technology (MIT)",
+        "dates": "May 2023 - October 2023",
+        "logo_info": `<img src="logos/MIT_logo_2.png" alt="Massachusetts Institute of Technology (MIT)" style="width: 100%; max-width: 300px; margin-bottom: 5px; margin-top: 5px; padding: 1px;">`,
+        "collapsible_content": `
+                          <p style="margin-bottom: 0px; margin-top: 10px;">
+                            <span style="font-weight: bold;">ECTS Credits:</span> 30 <br>
+                            <!--<span style="float:right; font-weight: bold;">GPA: ?/6</span><br>-->
+                            <!--<span align="justify" style="color: red;">Explain TFM</span>-->
+                          </p>
+                          <p align="justify" style="margin-bottom: -0px;">
+                            ETH Physics' Master Thesis conducted at MIT, under the supervision of Prof. Dr. Christoph M. E. Paus and Prof. Dr. Günther Dissertori, on rare decays of the Higgs boson into a photon and a vector meson.
+                          </p>
+                        `,
+    },
+    {
+        "degree" : "MSc ETH in Physics",
+        "institution" : "ETH Zürich",
+        "dates": "September 2021 - October 2023",
+        "logo_info": `<img src="logos/ETH_logo.png" alt="ETH Zürich" style="width: 100%; max-width: 300px; margin-bottom: 5px; margin-top: 5px; padding: 5px;">`,
+        "collapsible_content": `
+                          <p style="margin-bottom: 0px; margin-top: 10px;">
+                            <span style="font-weight: bold;">ECTS Credits:</span> 90
+                            <span style="float:right; font-weight: bold;">GPA: 5.6/6</span><br>
+                            <span align="justify">Courses taken:</span>
+                          </p>
+                          <ul style="margin-bottom: -0px;">
+                            <li>Quantum Field Theory I & II</li>
+                            <li>Phenomenology of Particle Physics I & II</li>
+                            <li>General Relativity</li>
+                            <li>Introduction to String Theory</li>
+                            <li>The Standard Model of Electroweak Interactions</li>
+                            <li>Proseminar in Relational Quantum Mechanics</li>
+                            <li>Semester Project in Beyond the Standard Model EFTs</li>
+                          </ul>
+                          <p align="justify" style="margin-bottom: -0px;">
+                            Master Thesis (30 ECTS) conducted at MIT on rare decays of the Higgs boson.
+                          </p>
+                        `,
+    },
+    {
+        "degree" : "BSc in Computer Science and Software Engineering",
+        "institution" : "UB Universitat de Barcelona",
+        "dates": "September 2016 - July 2021",
+        "logo_info": `<img src="logos/Logotip_UB.png" alt="UB Universitat de Barcelona" style="width: 100%; max-width: 300px;">`,
+        "collapsible_content": `
+                          <p style="margin-bottom: 0px; margin-top: 10px;">
+                            <span style="font-weight: bold;">ECTS Credits:</span> 240
+                            <span style="float:right; font-weight: bold;">GPA: 9.3/10</span><br>
+                            <span align="justify">Excellent with honours in:</span>
+                          </p>
+                          <ul style="margin-bottom: 0px;">
+                            <li style="margin-right: 10px;">Basic Digital Design</li>
+                            <li style="margin-right: 10px;">Software Design</li>
+                            <li style="margin-right: 10px;">Data Structure</li>
+                            <li style="margin-right: 10px;">Advanced Algorithms</li>
+                            <li style="margin-right: 10px;">Artificial Vision</li>
+                            <li style="margin-right: 10px;">Workshop on New Uses of Computers</li>
+                            <li style="margin-right: 10px;">Embedded Systems Programming</li>
+                            <li style="margin-right: 10px;">Operating Systems I</li>
+                            <li style="margin-right: 10px;">Operating Systems II</li>
+                            <li style="margin-right: 10px;">Networks</li>
+                            <li style="margin-right: 10px;">Human Factors and Computing</li>
+                            <li style="margin-right: 10px;">Graphics and Data Visualization</li>
+                          </ul>
+                          <p align="justify" style="margin-bottom: -0px;">
+                            Final project (18 ECTS): "<em><a href="http://hdl.handle.net/2445/182557" target="_blank">Algorithmic Causal Effect Identification</a></em>", grade: 10/10. <a href="https://arxiv.org/abs/2107.04632" target="_blank">Arxiv</a>.<br>
+                            Awarded an extraordinary award.<br>
+                            Awarded the Joaquim Font Award for the best academic record.
+                          </p>
+                        `,
+    },
+    {
+        "degree" : "BSc in Mathematics",
+        "institution" : "UB Universitat de Barcelona",
+        "dates": "September 2014 - July 2020",
+        "logo_info": `<img src="logos/Logotip_UB.png" alt="UB Universitat de Barcelona" style="width: 100%; max-width: 300px;">`,
+        "collapsible_content": `
+                          <p style="margin-bottom: 0px; margin-top: 10px;">
+                            <span style="font-weight: bold;">ECTS Credits:</span> 240
+                            <span style="float:right; font-weight: bold;">GPA: 8.4/10</span><br>
+                            <span align="justify">Excellent with honours in:</span>
+                          </p>
+                          <ul style="margin-bottom: 0px;">
+                            <li style="margin-right: 10px;">Introduction to Differential Calculus</li>
+                            <li style="margin-right: 10px;">Graphs</li>
+                            <li style="margin-right: 10px;">Topology and Global Differential Geometry of Surfaces</li>
+                          </ul>
+                          <p align="justify" style="margin-bottom: -0px;">
+                            Final project (18 ECTS): "<em><a href="http://hdl.handle.net/2445/177970" target="_blank">Global Dynamics of Newton's Method for Complex Polynomials</a></em>", grade: 9.7/10.<br>
+                          </p>
+                        `,
+    },
+    {
+        "degree" : "BSc in Physics",
+        "institution" : "UB Universitat de Barcelona",
+        "dates": "September 2014 - January 2020",
+        "logo_info": `<img src="logos/Logotip_UB.png" alt="UB Universitat de Barcelona" style="width: 100%; max-width: 300px;">`,
+        "collapsible_content": `
+                          <p style="margin-bottom: 0px; margin-top: 10px;">
+                            <span style="font-weight: bold;">ECTS Credits:</span> 240
+                            <span style="float:right; font-weight: bold;">GPA: 8.5/10</span><br>
+                            <span align="justify">Excellent with honours in:</span>
+                          </p>
+                          <ul style="margin-bottom: 0px;">
+                            <li style="margin-right: 10px;">Differential Equations and Vector Calculus</li>
+                            <li style="margin-right: 10px;">Mathematical Methods for Physics II</li>
+                            <li style="margin-right: 10px;">Quantum Physics</li>
+                            <li style="margin-right: 10px;">Solid State Physics</li>
+                            <li style="margin-right: 10px;">Classical Electrodynamics</li>
+                            <li style="margin-right: 10px;">Quantum Mechanics</li>
+                            <li style="margin-right: 10px;">Atomic Physics and Radiation</li>
+                          </ul>
+                          <p align="justify" style="margin-bottom: -0px;">
+                            Final project (6 ECTS): "<em><a href="https://github.com/pedemonte96/TFG_physics_2019" target="_blank">Machine Learning Approach to the Inverse Ising Problem</a></em>", grade: 8.9/10.<br>
+                          </p>
+                        `,
+    },
+    {
+        "degree" : "Secondary Education",
+        "institution" : "IE Costa i Llobera (Barcelona)",
+        "dates": "September 2012 - May 2014",
+        "logo_info": `<img src="logos/Costa_i_Llobera_logo.png" alt="IE Costa i Llobera" style="width: 100%; max-width: 300px;">`,
+        "collapsible_content": `
+                          <p align="justify" style="margin-bottom: -0px; margin-top: 10px;">
+                            Excellent with honours.<span style="float:right; font-weight: bold;">GPA: 9.3/10</span><br>
+                            Final project: "<em>Probabilities in gambling</em>", grade: 10/10.<br>
+                          </p>
+                        `,
+    },
+];
--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,39 @@
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
 
+import { educationData } from './education.js';
+import { experienceData } from './experience.js';
+import { skillsData } from './skills.js';
+
 $(document).ready(function() {
   document.getElementById("age").textContent = martisAge();
   AOS.init( {
     // uncomment below for on-scroll animations to played only once
     // once: true  
   }); // initialize animate on scroll library
+  createDivs();
+  setListeners();
 });
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
+function setListeners() {
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    //console.log(this.id);
-    //console.log(this.parentElement.parentElement.getElementsByClassName("content_collapsible")[0]);
-    this.classList.toggle("active_collapsible");
-    //const name = "collapse_content_" + this.id.replace('collapsible_button_','');
-    //console.log(name);
-    var content = this.parentElement.parentElement.getElementsByClassName("content_collapsible")[0];
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      //console.log(this.id);
+      //console.log(this.parentElement.parentElement.getElementsByClassName("content_collapsible")[0]);
+      this.classList.toggle("active_collapsible");
+      //const name = "collapse_content_" + this.id.replace('collapsible_button_','');
+      //console.log(name);
+      var content = this.parentElement.parentElement.getElementsByClassName("content_collapsible")[0];
+      if (content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
 }
 
 function martisAge() {
@@ -60,4 +68,111 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
   modal.style.display = "none";
+}
+
+function createDivs() {
+  var i;
+  //console.log(experienceData);
+  for (i = 0; i < experienceData.length; i++) {
+    createExperienceCard(experienceData[i]);
+  }
+
+  //console.log(educationData);
+  for (i = 0; i < educationData.length; i++) {
+    createEducationCard(educationData[i]);
+  }
+
+  //console.log(skillsData);
+  for (i = 0; i < skillsData.length; i++) {
+    createSkillsCard(skillsData[i]);
+  }
+}
+
+function createExperienceCard(elementInfo) {
+  document.getElementById("experience_container").innerHTML += "  ";
+  var div = document.createElement("div");
+  div.className = 'card';
+  div.innerHTML += `
+              <div class="row">
+                <div class="col-md-4 col-lg-3 bg-primary vertical-center" data-aos="fade-right" data-aos-offset="20" data-aos-duration="500">
+                  <div class="card-body cc-education-header">
+                    <p style="margin-bottom: 5px;">${elementInfo.dates}</p>
+                    ${elementInfo.logo_info}
+                  </div>
+                </div>
+                <div class="col-md-8 col-lg-9" data-aos="fade-left" data-aos-offset="20" data-aos-duration="500">
+                  <div class="card-body">
+                    <div style="display:flex;">
+                      <div style="width: 100%;">
+                        <div class="h5">${elementInfo.degree}</div>
+                        <p class="category" style="margin-bottom: -5px;">${elementInfo.institution}</p>
+                        <p align="justify" style="margin-bottom: 0px; margin-top: 10px;">${elementInfo.noncollapsible_text}</p>
+                        <div class="content_collapsible">${elementInfo.collapsible_content}</div>
+                      </div>
+                      <div style="width:auto; text-align:right; white-space: nowrap">
+                        <button class="collapsible"></button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            `;
+  document.getElementById("experience_container").appendChild(div);
+  document.getElementById("experience_container").innerHTML += "\n          ";
+}
+
+function createEducationCard(elementInfo) {
+  document.getElementById("education_container").innerHTML += "  ";
+  var div = document.createElement("div");
+  div.className = 'card';
+  div.innerHTML += `
+              <div class="row">
+                <div class="col-md-4 col-lg-3 bg-primary vertical-center" data-aos="fade-right" data-aos-offset="20" data-aos-duration="500">
+                  <div class="card-body cc-education-header">
+                    <p style="margin-bottom: 5px;">${elementInfo.dates}</p>
+                    ${elementInfo.logo_info}
+                  </div>
+                </div>
+                <div class="col-md-8 col-lg-9" data-aos="fade-left" data-aos-offset="20" data-aos-duration="500">
+                  <div class="card-body">
+                    <div style="display:flex;">
+                      <div style="width: 100%;">
+                        <div class="h5">${elementInfo.degree}</div>
+                        <p class="category" style="margin-bottom: -5px;">${elementInfo.institution}</p>
+                        <div class="content_collapsible">${elementInfo.collapsible_content}</div>
+                      </div>
+                      <div style="width:auto; text-align:right; white-space: nowrap">
+                        <button class="collapsible"></button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            `;
+  document.getElementById("education_container").appendChild(div);
+  document.getElementById("education_container").innerHTML += "\n          ";
+}
+
+function createSkillsCard(elementInfo) {
+  document.getElementById("skill_container").innerHTML += "  ";
+  var div = document.createElement("div");
+  div.className = 'row';
+  div.innerHTML += `
+                  <div class="col-md-6">
+                    <div class="progress-container progress-primary"><span class="progress-badge">${elementInfo.name1}</span>
+                      <div class="progress">
+                        <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: ${elementInfo.level1}%;"></div><span class="progress-value"></span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-6">
+                    <div class="progress-container progress-primary"><span class="progress-badge">${elementInfo.name2}</span>
+                      <div class="progress">
+                        <div class="progress-bar progress-bar-primary" data-aos="progress-full" data-aos-offset="10" data-aos-duration="2000" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: ${elementInfo.level2}%;"></div><span class="progress-value"></span>
+                      </div>
+                    </div>
+                  </div>
+                `;
+  document.getElementById("skill_container").appendChild(div);
+  document.getElementById("skill_container").innerHTML += "\n              ";
 }
